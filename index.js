4ba1cbf2ddd446ca465620b54ef189d34302e670
Nathan Kim <younggi@gmail.com>
2020-12-10 09:46:07 -0500

    updated config for prod vs dev

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const config = require('./config/keys')
+const { User } = require('./models/User')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://younggi:nathan22@cluster0.fjcfm.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
   }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요!!! ajaja')
 })
-https://runkit.com/embed/save
+
+app.post('/register', (req, res) => {
+  // get User registration info from the client, then put into DB
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
